af3e43d78d28d3813684a7e0c1e1024e0dd80eb7
Sungin <jungs@uchicago.edu>
2021-09-13 23:55:27 -0700

    Player Animation
    Implement player animation code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] int movementSpeed;
-    
+
     private float h;
     private float v;
     private bool isHorizontal;
 
     Rigidbody2D rigid;
+    Animator animator;
 
-    private void Awake() 
+    private void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();    
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,14 +27,29 @@ public class Player : MonoBehaviour
 
         bool hDown = Input.GetButtonDown("Horizontal");
         bool vDown = Input.GetButtonDown("Vertical");
-        bool hUp= Input.GetButtonUp("Horizontal");
-        bool vUp= Input.GetButtonUp("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
 
-        if (hDown || vUp)
+        if (hDown)
             isHorizontal = true;
-        else if (vDown || hUp)
+        else if (vDown)
             isHorizontal = false;
-        
+        else if (hUp || vUp)
+            isHorizontal = h != 0;
+
+        if (animator.GetInteger("hAxisRaw") != h)
+        {
+            animator.SetBool("directionChanged", true);
+            animator.SetInteger("hAxisRaw", (int)h);
+        }
+            
+        else if (animator.GetInteger("vAxisRaw") != v)
+        {
+            animator.SetBool("directionChanged", true);
+            animator.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+            animator.SetBool("directionChanged", false);
 
     }
 
