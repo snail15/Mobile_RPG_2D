b6ae73f3fd0902bd5539cb475db0fa34895df1e2
Sungin <jungs@uchicago.edu>
2021-09-16 17:09:08 -0700

    NPC Dialog
    Implement NPC dialog with portrait

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -4,23 +4,46 @@ using UnityEngine;
 
 public class DialogManager : MonoBehaviour
 {
+    [SerializeField] Sprite[] portaits;
+
     Dictionary<int, string[]> dialogData;
+    Dictionary<int, Sprite> portraitData;
 
     private void Awake()
     {
         dialogData = new Dictionary<int, string[]>();
+        portraitData = new Dictionary<int, Sprite>(); ;
         GenerateDialogData();
     }
 
     public string GetDialog(int objId, int dialogIdx)
     {
+        if (dialogIdx == dialogData[objId].Length)
+            return null;
+
         return dialogData[objId][dialogIdx];
+    }
+
+    public Sprite GetPortait(int objId, int portraitIdx)
+    {
+        return portraitData[objId + portraitIdx];
     }
 
     private void GenerateDialogData()
     {
-        dialogData.Add(1000, new string[] { "안녕?", "이 곳에 처음 왔구나?" });
+        dialogData.Add(1000, new string[] { "안녕?:0", "이 곳에 처음 왔구나?:1" });
+        dialogData.Add(2000, new string[] { "여어..:1", "이 호수는 정말 아름답지?:0", "사실 이 호수에는 비밀이 있다고해..:1" });
         dialogData.Add(100, new string[] { "평범한 나무상자다.." });
         dialogData.Add(200, new string[] { "누군가 사용했던 흔적이 있는 책상이다" });
+
+        portraitData.Add(1000 + 0, portaits[0]);
+        portraitData.Add(1000 + 1, portaits[1]);
+        portraitData.Add(1000 + 2, portaits[2]);
+        portraitData.Add(1000 + 3, portaits[3]);
+        portraitData.Add(2000 + 0, portaits[4]);
+        portraitData.Add(2000 + 1, portaits[5]);
+        portraitData.Add(2000 + 2, portaits[6]);
+        portraitData.Add(2000 + 3, portaits[7]);
+
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     [Header("UI Components")]
     [SerializeField] Text dialogText;
     [SerializeField] GameObject dialogPanel;
+    [SerializeField] Image portrait;
 
     [Header("Managers")]
     [SerializeField] DialogManager dialogManager;
@@ -20,33 +21,37 @@ public class GameManager : MonoBehaviour
 
     public void Scan(GameObject scannedObj)
     {
-        if (isScanning)
-        {
-            isScanning = false;
-        }
-        else
-        {
-            isScanning = true;
-            scannedObject = scannedObj;
-            ObjData objData = scannedObject.GetComponent<ObjData>();
-            Talk(objData.id, objData.isNPC);
-        }
+        scannedObject = scannedObj;
+        ObjData objData = scannedObject.GetComponent<ObjData>();
+        Talk(objData.id, objData.isNPC);
 
         dialogPanel.SetActive(isScanning);
-
-
     }
 
     private void Talk(int id, bool isNPC)
     {
         string dialog = dialogManager.GetDialog(id, dialogIdx);
+        
+        if (dialog == null)
+        {
+            isScanning = false;
+            dialogIdx = 0;
+            return;
+        }
+
         if (isNPC)
         {
-            dialogText.text = dialog;
+            string[] splitTxt = dialog.Split(':');
+            dialogText.text = splitTxt[0];
+            portrait.sprite = dialogManager.GetPortait(id, int.Parse(splitTxt[1]));
+            portrait.color = new Color(1, 1, 1, 1);
         }
         else
         {
             dialogText.text = dialog;
+            portrait.color = new Color(1, 1, 1, 0);
         }
+        isScanning = true;
+        dialogIdx += 1;
     }
 }
